cc7713e350ab44e80ece7afa68521f213017ea49
Niro Karunakaran <niro@tailit.com>
2021-07-25 15:29:03 +0200

    changed learning rate parameter and added comments

--- housingprices.py
+++ housingprices.py
@@ -29,14 +29,17 @@ col_list = ['kvmpris', 'bydel_navn', 'aar']
 
 df = pd.read_csv(filename, encoding='latin1', usecols=col_list, sep=';')
 oslo = (df.loc[df['bydel_navn'].isin(['Oslo i alt'])])
+# creates python list for square feet price and years in Oslo
 oslo_kvm = [x[2] for x in oslo.values.tolist()]
 oslo_aar = [int(x[0]) for x in oslo.values.tolist()]
+# Change to pandas datafram list so it can be passed as parameters in Keras functions line 43
 df_oslo_aar = pd.DataFrame(oslo_aar, columns=['aar'])
 df_oslo_kvm = pd.DataFrame(oslo_kvm, columns=['kvmpris'])
 
+# train and compile the dataset with chosen parameters. Can be changed when trying to optimize results
 model = keras.Sequential()
 model.add(keras.layers.Dense(1, input_shape=(1,)))
-model.compile(keras.optimizers.Adam(learning_rate=.005), 'mean_squared_error')
+model.compile(keras.optimizers.Adam(learning_rate=1), 'mean_squared_error')
 model.fit(x=df_oslo_aar, y=df_oslo_kvm, epochs=30, batch_size=10)
 
 new_year = 2022
